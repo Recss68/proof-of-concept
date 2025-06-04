7dc245edb308edca53bc217e891af1d1b4dd6026
Recep <recep.yildiz@hva.nl>
2025-06-04 13:05:12 +0200

    #4 Server.js Base and ( error, views, route)

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+app.set('views', './views')
+
+
+app.get('/', async function (request, response) {
+    try {
+    
+    
+        response.render('liquid.liquid', {
+  
+        });
+      } catch (error) {
+        console.error("Something went wrong in the page check error:",error);
+        response.status(500).render("error.liquid");
+      }
+    });
+
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+  console.log(`Project draait via http://localhost:${app.get('port')}/`)
+})
