0f4c6e961c74b279c7b4e53d99ef3f99dcc6683f
Recep <recep.yildiz@hva.nl>
2025-06-16 16:43:55 +0200

    #22 Werkende script geschreven voor de class loading

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,26 @@
-// Hamburger menu 
-let menu = document.querySelector('.hamburger-menu');
-let menuItems = document.querySelector('.menu-items');
+// Deze script zorgt ervoor dat de css classes voor de hamburger menu pas worden ingeladen
+// Als de js het doet, doet de js het niet = geen hamburger menu
 
-menu.addEventListener('click', openMenu)
-
-function openMenu(){
-    menuItems.classList.toggle('menuActive')
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const ul = document.querySelector("ul.menufb");
+    if (ul) {
+      ul.classList.remove("menufb");
+      ul.classList.add("menu-items");
+    }
+  
+    const listItems = document.querySelectorAll("li.itemfb");
+    listItems.forEach(function(li) {
+      li.classList.remove("itemfb");
+      li.classList.add("nav-item");
+    });
+  
+    // Hamburger menu
+    const menu = document.querySelector('.hamburger-menu');
+    const menuItems = document.querySelector('.menu-items');
+  
+    if (menu && menuItems) {
+      menu.addEventListener('click', function () {
+        menuItems.classList.toggle('active');
+      });
+    }
+  });
