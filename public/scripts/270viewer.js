0648f26f1dff60fb673f2343207f90564915b6ab
Recep <recep.yildiz@hva.nl>
2025-06-18 11:06:44 +0200

    #19 Made the viewer script

--- public/scripts/270viewer.js
+++ public/scripts/270viewer.js
@@ -1,14 +1,31 @@
-TSDViewer.create(document.querySelector('.myOwnDiv'), {
-    model: 'hva-pictureframe1-horizontal',
-    width: '500px',
-    height: '500px',
+if (typeof TSDViewer !== 'undefined') {
+    if(TSDViewer){
+ 
+
+let $wanttobemodel = document.querySelector('#wanttobemodel');
+$wanttobemodel.querySelector('img').setAttribute('style', 'display: none;');
+$wanttobemodel.setAttribute('style', 'border: none;')
+
+TSDViewer.create($wanttobemodel, {
+    model: 'hva-pictureframe3-horizontal',
+    width: '800px',
+    height: '800px',
+    cameraZoomLevel: 0.5,
+    cameraPosition: '0',
+    plugins: 'customiser',
     onCreate: () => {
         // viewer was created
     },
     onLoadComplete: () => {
         // loaded model
+        $wanttobemodel.setTexture({name: 'field1', source: document.querySelector('#wanttobemodel img'),scale: 'cover'}).then(() => { /* callback */ });
+        $wanttobemodel.setBackground("#4b4c54");
+        $wanttobemodel.querySelector('img').remove();
     },
     onLoadFailure: () => {
         // something's wrong, load fallback
-    }
+    }   
+
 })
+
+    }}
