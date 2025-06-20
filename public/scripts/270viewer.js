cf1a1fe551031f15d9567b8b2d1d7458610a3e88
Recep <recep.yildiz@hva.nl>
2025-06-20 22:53:39 +0200

    viewer aanpassing

--- public/scripts/270viewer.js
+++ public/scripts/270viewer.js
@@ -6,8 +6,25 @@ if (typeof TSDViewer !== 'undefined') {
     $wanttobemodel.querySelector('img').setAttribute('style', 'display: none;');
     $wanttobemodel.setAttribute('style', 'border: none;')
 
+    let paintingFullImg = document.querySelector('#wanttobemodel img');
+
+    // Check if the image is orientation: landscape or portait, and adjust the chosen model based on that calc
+    var width = paintingFullImg.naturalWidth;
+    var height = paintingFullImg.naturalHeight;
+    let zoomLvl;
+    if (width > height){
+        modelOrientation = 'hva-pictureframe3-horizontal' /* landscape */
+    } else if (width < height){
+        modelOrientation = 'hva-pictureframe3-vertical' /* portrait */
+    } else {
+        modelOrientation = 'hva-pictureframe2' /* vierkant */
+    }
+    if (height === undefined || width === undefined){
+        modelOrientation = 'hva-pictureframe3-horizontal' /* fallback */
+    }
+
     TSDViewer.create($wanttobemodel, {
-        model: 'hva-pictureframe3-horizontal',
+        model: modelOrientation,
         width: '100vw',
         height: '100%',
         cameraZoomLevel: 0.5,
